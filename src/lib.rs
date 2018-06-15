e4e47268293496203893f2aa8168823ea6c24665
Valentin Roland <valentin@vroland.de>
2018-06-15 16:42:32 +0200

    move mfnf-specific spec to new repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 //! This library provides common, Mathe-für-Nicht-Freaks specific code.
 
 extern crate mediawiki_parser;
-extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate mwparser_utils_derive;
 
 #[macro_use]
-pub mod util;
+mod util;
 pub mod transformations;
-mod spec;
 
-pub use spec::*;
-
+pub use util::*;
